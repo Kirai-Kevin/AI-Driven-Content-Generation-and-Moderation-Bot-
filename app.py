cac2bc379a83666cd0f7d2bb5f155dfcbbd65ee3
Kirai-Kevin <167949441+Kirai-Kevin@users.noreply.github.com>
2024-07-25 09:35:02 +0300

    home

--- app.py
+++ app.py
@@ -216,5 +216,4 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=True)
